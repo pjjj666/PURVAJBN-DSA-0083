d5a1ee44604b0c711e716f92fdb0069cc4054f38
pjjj666 <purvajbnbca25@rvu.edu.in>
2026-03-11 23:14:31 +0530

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+int data;
+struct node *next;
+};
+
+int main()
+{
+struct node *head,*second,*temp;
+head=(struct node*)malloc(sizeof(struct node));
+second=(struct node*)malloc(sizeof(struct node));
+
+head->data=10;
+head->next=second;
+
+second->data=20;
+second->next=head;
+
+struct node *newnode;
+newnode=(struct node*)malloc(sizeof(struct node));
+
+newnode->data=5;
+
+temp=head;
+while(temp->next!=head)
+{
+temp=temp->next;
+}
+
+newnode->next=head;
+temp->next=newnode;
+head=newnode;
+
+temp=head;
+
+do
+{
+printf("%d ",temp->data);
+temp=temp->next;
+}while(temp!=head);
+
+return 0;
+}
---
+++ 4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+int data;
+struct node *next;
+};
+
+int main()
+{
+struct node *head,*second,*temp;
+
+head=(struct node*)malloc(sizeof(struct node));
+second=(struct node*)malloc(sizeof(struct node));
+
+head->data=10;
+head->next=second;
+
+second->data=20;
+second->next=NULL;
+
+temp=head;
+
+head=head->next;
+
+free(temp);
+
+temp=head;
+
+while(temp!=NULL)
+{
+printf("%d ",temp->data);
+temp=temp->next;
+}
+
+return 0;
+}
